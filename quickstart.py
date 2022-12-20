059ed5457e1b21cecdbf215c9d9b734be957bf65
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2022-12-20 13:31:41 -0600

    Add files via upload

---
+++ quickstart.py
@@ -0,0 +1,13 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
+
+with open('drivelinks.txt', 'r') as file:
+	lines = file.readlines()
+	for line in lines:
+		fileid= line.strip()
+		file6 = drive.CreateFile({'id': fileid})
+		file6.GetContentFile(f"{line}.pdf")
