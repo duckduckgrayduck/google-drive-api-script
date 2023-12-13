6b60ec9144dc5b7fda5555efdb626f9a582d08df
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2023-12-13 08:36:13 -0600

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -1,13 +1,16 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+import re
 
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
 drive = GoogleDrive(gauth)
 
+url_pattern = re.compile(r'https://drive\.google\.com/file/d/([a-zA-Z0-9_-]+)/')
+
 with open('new.txt', 'r') as file:
 	lines = file.readlines()
 	for line in lines:
-		fileid= line.strip()
+		fileid= url_pattern.search(line).group(1)
 		file6 = drive.CreateFile({'id': fileid})
-		file6.GetContentFile(f"{line}.pdf")
+		file6.GetContentFile(f"{fileid}.pdf")
